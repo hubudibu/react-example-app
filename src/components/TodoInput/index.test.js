4ec37a8268a79094e43416a62147de0bcaf27a74
Fejes Aniko <oranjje@gmail.com>
2017-05-11 00:13:41 +0200

    feat(jest-tests): component snapshot test and state test for todoinput

---
+++ src/components/TodoInput/index.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
+import TodoInput from './';
+
+
+it('TodoInput renders as in snapshot', () => {
+  const wrapper = renderer.create(<TodoInput onSubmit={()=>{}} />).toJSON();
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('TodoInput value changes state', () => {
+  const todoText = 'Sample todo';
+  const input = mount(<TodoInput onSubmit={()=>{}} />);
+  input.simulate('change', {target: {value: todoText}});
+  expect(input.state()).toEqual({value: todoText});
+});
