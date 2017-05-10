46e1d9ba912444a99d3c806a35b834dd2cf68dfc
Fejes Aniko <oranjje@gmail.com>
2017-05-10 21:36:59 +0200

    feat(redux-todos): add store provider and initial state with todo reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = [
+  {
+    text: "Sleep at least 7 hours",
+    checked: false
+  }
+];
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+};
+
+export default todos;
