1c81987370385b5d2329330c0ef9edf132220554
Aniko Fejes <oranjje@gmail.com>
2017-05-17 21:19:13 +0200

    feat(thunk): install thunk, apply middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,26 @@ export const addTodo = (text) => {
     text
   };
 };
+
+const asyncStarted = () => {
+  return {
+    type: 'ASYNC_STARTED'
+  }
+}
+
+const asyncLoaded = () => {
+  return {
+    type: 'ASYNC_LOADED'
+  }
+}
+
+export const asyncAction = () => {
+  return dispatch => {
+
+    dispatch(asyncStarted());
+
+    setTimeout(()=>{
+      dispatch(asyncLoaded());
+    },1000);
+  }
+}
