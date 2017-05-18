90c6b4aca70892f798bf504d3ace73b8e1c35c8e
Aniko Fejes <oranjje@gmail.com>
2017-05-18 08:55:58 +0200

    feat(hoc): add react-redux-firebase

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import todos from './todos';
 import loading from './loading';
+import { firebaseStateReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
   todos,
-  loading
+  loading,
+  firebase: firebaseStateReducer
 });
 
 export default rootReducer;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { reactReduxFirebase } from 'react-redux-firebase';
 
-const store = createStore(rootReducer, composeWithDevTools(
+const config = {
+    apiKey: "AIzaSyCbIpLKw2IjUN7SBAqdqve2uE5Jyw3ZyV8",
+    authDomain: "react-todo-example-13004.firebaseapp.com",
+    databaseURL: "https://react-todo-example-13004.firebaseio.com"
+};
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(config, { userProfile: 'users' }),
+)(createStore)
+
+const store = createStoreWithFirebase(rootReducer, composeWithDevTools(
   applyMiddleware(thunk)
 ));
 
